b0231136497e925974f096deadb0d139f9293588
dcxia <dcxia.zq@gmail.com>
2021-02-06 01:08:37 -0500

    Finished 2.3

--- resources/learncpp/examples/chapter2/ch2.cpp
+++ resources/learncpp/examples/chapter2/ch2.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 
+
+////////////////////////////////////////////////////////////////////////////////
 void printA(){
     std::cout<< "A\n";
 }
@@ -12,7 +14,26 @@ void learningCPP2_2(){
     // std::cout << printA() << "\n";
 
 }
+////////////////////////////////////////////////////////////////////////////////
+int getValueFromUser(){
+    std::cout << "Enter an Integer: ";
+    int input{};
+    std::cin >> input;
 
+    return input;
+}
+
+void printDouble(int value){
+        std::cout << value << " doubled is: " << value * 2 <<"\n";
+}
+
+int main2_3(){
+    printDouble(getValueFromUser());
+    return 0;
+}
+
+////////////////////////////////////////////////////////////////////////////////
 int main(){
+    //main2_3();
     return 0;
 }
